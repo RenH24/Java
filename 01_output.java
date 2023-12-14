ad937184d901b8806cd7ddc851c46d3be68d29f0
KosarevMax <152382925+KosarevMax@users.noreply.github.com>
2023-12-14 10:08:08 +0400

    Create 01_output.java

---
+++ 01_output.java
@@ -0,0 +1,19 @@
+/* В программе заданы строка и несколько чисел. Выведите информацию на консоль так, как показано в примере.
+
+Sample Output:
+  С Днем Рождения, Андрей!
+  Сегодня Вам 23!
+  По этому поводу Вам скидка 15.5%
+  на весь ассортимент нашего сайта! */
+
+  class Main {
+    public static void main(String[] args) {
+        String name = "Андрей";
+        int age = 23;
+        double discount = 15.5;
+        System.out.println("С Днем Рождения, "+name+"!");
+        System.out.println("Сегодня Вам "+age+"!");
+        System.out.println("По этому поводу Вам скидка "+discount+"%");
+        System.out.println("на весь ассортимент нашего сайта!");
+    }
+}
