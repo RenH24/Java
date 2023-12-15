18b219512be3991d59e54f7f90095ec7802d9e20
Maxim <152382925+RenH24@users.noreply.github.com>
2023-12-15 09:19:35 +0400

    Create 16_day_of_the_week.java

---
+++ Stepik/16_day_of_the_week.java
@@ -0,0 +1,48 @@
+Напишите программу- переводчик. Пользователь вводит название дня недели на русском языке, а программа выводит это название на английском. 
+При этом на русском можно вводить название как с большой буквы, так и с маленькой. 
+А на английском день недели - имя собственное, поэтому выводится с большой буквы.
+
+Если  название дня недели введено неверно, то программа выводит "ERROR".
+Используйте оператор switch.
+
+// put your code here
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String day = scan.next();
+        switch (day){
+            case "Понедельник":
+            case "понедельник":
+                System.out.println("Monday");
+                break;
+            case "Вторник":
+            case "вторник":
+                System.out.println("Tuesday");
+                break;
+            case "Среда":
+            case "среда":
+                System.out.println("Wednesday");
+                break;
+            case "Четверг":
+            case "четверг":
+                System.out.println("Thursday");
+                break;
+            case "Пятница":
+            case "пятница":
+                System.out.println("Friday");
+                break;
+            case "Суббота":
+            case "суббота":
+                System.out.println("Saturday");
+                break;
+            case "Воскресенье":
+            case "воскресенье":
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("ERROR");
+        }
+    }
+}  
