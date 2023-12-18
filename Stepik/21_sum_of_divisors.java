b069debaa7d3a86178b1d68dae39c4e3d04c37e2
Maxim <152382925+RenH24@users.noreply.github.com>
2023-12-18 08:50:00 +0400

    Create 21_sum_of_divisors.java

---
+++ Stepik/21_sum_of_divisors.java
@@ -0,0 +1,24 @@
+Выполните задачу с использованием цикла for.
+Найти сумму делителей целого положительного числа. Например, вводится число 12. Сумма его делителей: 1+2+3+4+6+12=28. 
+
+Sample Input:
+12
+Sample Output:
+28
+
+// put your code here
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int number = scan.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= number; i++){
+            if (number % i == 0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
