09c39f62ed96180c7803a29182fc6c1789110fa1
Maxim <152382925+RenH24@users.noreply.github.com>
2024-01-12 10:28:04 +0400

    Create 64_upper_case.java

---
+++ Stepik/64_upper_case.java
@@ -0,0 +1,27 @@
+Введите строку, состоящую из слов, отделенных ровно одним пробелом.
+Замените первую букву каждого слова на прописную. Выведите полученную строку.
+
+Sample Input:
+one apple a day keeps a doctor away
+Sample Output:
+One Apple A Day Keeps A Doctor Away
+
+//put your code here
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        String[] words = text.split(" +");
+        String result = "";
+        for(int i = 0; i < words.length; i++) {
+            String symb1 = words[i].substring(0, 1).toUpperCase();
+            words[i] = symb1 + words[i].substring(1);
+            result += words[i];
+            result += " ";
+        }
+        result = result.trim();
+        System.out.println(result);
+    }
+}
