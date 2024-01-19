7ff21d70014d2680ec927aa8e1f895455c9922b0
Maxim <dredd20097@gmail.com>
2024-01-19 11:21:15 +0400

    Changes

--- src/main/codewars/6kyu_Decode Morse code.java
+++ src/main/codewars/6kyu_Decode Morse code.java
@@ -1,4 +1,4 @@
-/* 
+package codewars;/*
 In this kata you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice and digital data communication channels, it still has its use in some applications around the world.
 The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
 
@@ -14,12 +14,12 @@ MorseCodeDecoder.decode(".... . -.--   .--- ..- -.. .")
 //should return "HEY JUDE"
 */
 
-public class MorseCodeDecoder {
+class MorseCodeDecoder {
   public static String decode(String morseCode) {
     // your brilliant code here, remember that you can access the preloaded Morse code table through MorseCode.get(code)
     StringBuilder mc = new StringBuilder();
     for (String code : morseCode.trim().replace("   ", " / ").split(" ")) {
-      mc.append(code.equals("/") ? " " : MorseCode.get(code));
+      //mc.append(code.equals("/") ? " " : MorseCode.get(code));
     }
     return mc.toString();
     }
