b258b926cdcf3e2c2f95a3e7c120ccd5b3d2a2bb
Maxim <152382925+RenH24@users.noreply.github.com>
2024-01-23 10:09:10 +0400

    Create 05_Abiturient2.java

---
+++ src/main/stepik2/05_Abiturient2.java
@@ -0,0 +1,104 @@
+
+/*
+Доработайте класс Abiturient, созданный в п.1.2:
+Сделайте все поля приватными согласно принципу инкапсуляции;
+Добавьте в класс три разных конструктора, геттеры и сеттеры.
+Должен заработать код, представленный в классе Main. Этот код менять нельзя!
+
+Sample Input:
+Семенов 70 60 57
+  
+Sample Output:
+Семенов : 0.0
+Петров : 80.0
+Алексиевич : 62.3
+*/
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Abiturient[] list = new Abiturient[3];
+        list[0] = new Abiturient();
+        list[1] = new Abiturient("Петров", "Андрей", 90, 70, 80);
+        list[2] = new Abiturient("Алексиевич", "Анна");
+        list[0].setSurname(scan.next());
+        list[2].setPhysics(scan.nextInt());
+        list[2].setMath(scan.nextInt());
+        list[2].setLang(scan.nextInt());
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != null) {
+                System.out.printf("%s : %.1f\n", list[i].getSurname(), list[i].average());
+            }
+        }
+    }
+}
+
+class Abiturient {
+    private String surname;
+    private String name;
+    private int math;
+    private int physics;
+    private int lang;
+
+    public Abiturient() {
+    }
+    
+    public Abiturient(String surname, String name) {
+        this.surname = surname;
+        this.name = name;
+    }
+    
+    public Abiturient(String surname, String name, int math, int physics, int lang) {
+        this.surname = surname;
+        this.name = name;
+        this.math = math;
+        this.physics = physics;
+        this.lang = lang;
+    }
+
+    double average() {
+        return (math + physics + lang) / 3.;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMath() {
+        return math;
+    }
+
+    public void setMath(int math) {
+        this.math = math;
+    }
+
+    public int getPhysics() {
+        return physics;
+    }
+
+    public void setPhysics(int physics) {
+        this.physics = physics;
+    }
+
+    public int getLang() {
+        return lang;
+    }
+
+    public void setLang(int lang) {
+        this.lang = lang;
+    }
+}
