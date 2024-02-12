e0018af95c9038d4ec07339ae535993491f5512e
Maxim <dredd20097@gmail.com>
2024-02-12 15:03:56 +0400

    Changes

--- src/main/stepik2/33_UniversalStack.java
+++ src/main/stepik2/33_UniversalStack.java
@@ -1,4 +1,4 @@
-
+package stepik2;
 /*
 Помните класс Stack, который Вы создавали в п 3.3? В этой задаче нужно его усовершенствовать: сделать так, чтобы он мог работать не только с символами, но и с объектами любого класса!!!
 Отдельно добавьте класс Student с полями: фамилия, средний балл, возраст.
@@ -18,16 +18,16 @@ Sample Output:
 
 import java.util.Scanner;
 
-class Main {
+class Main33 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Stack<Student> stack = new Stack<>(10);
-        
+        Stack2<Student3> stack = new Stack2<>(10);
+
         String buffer = scan.nextLine();
         while (!"Конец".equals(buffer)) {
             try {                
                 String[] parts = buffer.split(" ");
-                stack.push(new Student(parts[0], Double.parseDouble(parts[1]), Integer.parseInt(parts[2])));
+                stack.push(new Student3(parts[0], Double.parseDouble(parts[1]), Integer.parseInt(parts[2])));
             } catch (StackException ex) {
                 System.out.println(ex.getMessage());
             }
@@ -37,12 +37,12 @@ class Main {
     }
 }
 
-class Student {
+class Student3 {
     private String name;
     private double ball;
     private int age;
 
-    public Student(String name, double ball, int age) {
+    public Student3(String name, double ball, int age) {
         this.name = name;
         this.ball = ball;
         this.age = age;
@@ -54,20 +54,20 @@ class Student {
     }
 }
 
-class StackException extends Exception{
-    public StackException() {
+class StackException2 extends Exception{
+    public StackException2() {
     }
 
-    public StackException(String message) {
+    public StackException2(String message) {
         super(message);
     }
 }
 
-class Stack <E> {
+class Stack2 <E> {
     E[] array;
     private int top;
 
-    public Stack(int n) {
+    public Stack2(int n) {
         array = (E[]) new Object[n];
         top = 0;
     }
--- src/main/stepik2/34_Purchase2.java
+++ src/main/stepik2/34_Purchase2.java
@@ -1,4 +1,4 @@
-
+package stepik2;
 /*
 Усовершенствуйте Васину программу так, чтобы в списке покупок могли быть как обычные товары, так и товары со скидкой. 
 При этом в каждой строке сначала вводится тип класса (1 - класс Purchase, 2 - класс DiscountPurchase), а затем уже идут необходимые поля.
@@ -17,17 +17,17 @@ Total sum = 28700
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Main {
+class Main34 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayList<Purchase> list = new ArrayList<>();
+        ArrayList<Purchase4> list = new ArrayList<>();
 
         String buffer = scan.nextLine();
         while (!"end".equals(buffer)) {
             String[] data = buffer.split(" ");
             int type = Integer.parseInt(data[0]);
             if (type == 1) {
-                list.add(new Purchase(data[1], Integer.parseInt(data[2]), Integer.parseInt(data[3])));
+                list.add(new Purchase4(data[1], Integer.parseInt(data[2]), Integer.parseInt(data[3])));
             } else {
                 list.add(new DiscountPurchase(data[1], Integer.parseInt(data[2]), Integer.parseInt(data[3]), Integer.parseInt(data[4])));
             }
@@ -37,7 +37,7 @@ class Main {
         System.out.println("Total sum = " + getTotalSum(list));
     }
 
-    public static int getTotalSum(ArrayList<Purchase> list) {
+    public static int getTotalSum(ArrayList<Purchase4> list) {
         int sum = 0;
         for (int i = 0; i < list.size(); i++) {
             sum += list.get(i).getCost();
@@ -46,18 +46,18 @@ class Main {
     }
 }
 
-class Purchase {
+class Purchase4 {
     private String name;
     private int price;
     private int number;
 
-    public Purchase(String name, int price, int number) {
+    public Purchase4(String name, int price, int number) {
         this.name = name;
         this.price = price;
         this.number = number;
     }
 
-    public Purchase() {
+    public Purchase4() {
     }
 
     int getCost() {
@@ -65,7 +65,7 @@ class Purchase {
     }
 }
 
-class DiscountPurchase extends Purchase {
+class DiscountPurchase extends Purchase4 {
     private int discount;
 
     public DiscountPurchase(String name, int price, int number, int discount) {
