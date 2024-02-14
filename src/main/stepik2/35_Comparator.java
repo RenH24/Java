4604da5225ab989d726802c7d1acd21f6e5fd08f
Maxim <152382925+RenH24@users.noreply.github.com>
2024-02-14 08:39:41 +0400

    Create 35_Comparator.java

---
+++ src/main/stepik2/35_Comparator.java
@@ -0,0 +1,94 @@
+
+/*
+Разработать класс Purchase, описывающий покупку товара:
+Поля: название товара; цена в копейках; кол-во единиц товара.
+В классе Main создать массив покупок, данные вводятся с консоли. В первой строке вводится количество элементов массива. В последующих строках  - данные каждой покупки, разделенные пробелами.
+Отсортировать  массив покупок по названию товара и вывести на консоль. Для каждого объекта выводятся: название товара, цена, количество, стоимость в копейках (отделенные пробелами). 
+Затем отсортировать по стоимости покупки (стоимость=цена*количество) и после пустой строки снова вывести на консоль. Использовать метод sort() класса Arrays.
+
+Sample Input:
+3
+Молоко 169 2
+Сахар 269 1
+Кофе 695 1
+Sample Output:
+Кофе 695 1 695
+Молоко 169 2 338
+Сахар 269 1 269
+
+Сахар 269 1 269
+Молоко 169 2 338
+Кофе 695 1 695
+*/
+
+import java.util.Comparator;
+import java.util.Scanner;
+import static java.util.Arrays.sort;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //создание массива и ввод данных
+        Purchase[] list = new Purchase[scan.nextInt()];
+
+        for (int i = 0; i < list.length; i++) {
+            list[i] = new Purchase(scan.next(), scan.nextInt(), scan.nextInt());
+        }
+        //сортировка по названию и вывод на консоль
+        sort(list, new Comparator<Purchase>() {
+            @Override
+            public int compare(Purchase o1, Purchase o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        for (Purchase item: list) {
+            System.out.println(item);
+        }
+        System.out.println();
+        //сортировка по стоимости
+        sort(list, new Comparator<Purchase>() {
+            @Override
+            public int compare(Purchase o1, Purchase o2) {
+                return o1.getValue() - o2.getValue();
+            }
+        });
+        for (Purchase item: list) {
+            System.out.println(item);
+        }
+    }
+}
+
+class Purchase {
+    private String name;
+    private int price;
+    private int number;
+
+    public Purchase(String name, int price, int number) {
+        this.name = name;
+        this.price = price;
+        this.number = number;
+    }
+
+    public int getValue() {
+        return getPrice() * getNumber();
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + price + " " + number + " " + getValue();
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
