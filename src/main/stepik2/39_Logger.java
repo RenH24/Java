c01c7e25b127f03ef45325922e50ee8f15dc3558
Maxim <152382925+RenH24@users.noreply.github.com>
2024-02-22 10:20:34 +0400

    Create 39_Logger.java

---
+++ src/main/stepik2/39_Logger.java
@@ -0,0 +1,57 @@
+
+/*
+Логгер - это класс, который будет записывать ошибки во внутреннюю строку методом putError() по принципу стека (т.е. последняя ошибка будет в этой строке первой). 
+Метод getErrors() позволяет получить список ошибок в виде итоговой строки.
+Логгер должен быть единственным в программе. Т.е. сколько бы раз мы его не создавали, это должен быть один и тот же логгер!
+
+Sample Input:
+34 67 proba 45 end
+Sample Output:
+45 proba 67 34
+*/
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Logger logger = Logger.getInstance();
+
+        String text = scan.next();
+        while (!"end".equals(text)) {
+            logger = Logger.getInstance();
+            logger.putError(text);
+            text = scan.next();
+        }
+
+        System.out.println(logger.getErrors());
+    }
+}
+
+class Logger {
+    private StringBuilder errors;
+    private static Logger item;
+    
+    private Logger() {
+        errors = new StringBuilder();
+    }
+    
+    public static Logger getInstance() {
+        if (item == null) {
+            synchronized (Logger.class) {
+                if (item == null) {
+                    item = new Logger();
+                }
+            }
+        }
+        return item;
+    }
+    
+    public void putError(String str) {
+        errors.insert(0, " ").insert(0, str);
+    }
+    
+    public String getErrors() {
+        return errors.toString().trim();
+    }
+}
